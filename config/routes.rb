34dbe5d2f9159230a4bda36f8be5829a6a02b8ee
Simone Westphal <simone.westphal@me.com>
2015-04-05 09:00:55 +0200

    small fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "pages#home"
+  root "application#hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
