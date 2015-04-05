ac82f3d88e96983244dfc53ba4e97c28ba68c429
Simone Westphal <simone.westphal@me.com>
2015-04-05 17:34:54 +0200

    Add stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home' => 'pages#home'
+  root "pages#erb_demo"
 
-  root "application#hello"
+  get "pages/home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
