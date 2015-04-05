c749b768cdb8670fc81218ae0f0e991bb1cdf5b9
Simone Westphal <simone.westphal@me.com>
2015-04-05 09:04:05 +0200

    small fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home' => 'pages#home'
 
   root "application#hello"
   # The priority is based upon order of creation: first created -> highest priority.
